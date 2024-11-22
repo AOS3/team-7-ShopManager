60cec6cbe0f1f774a550fa4984dbfc435db04513
ok009 <skgmlwls77@gmail.com>
2024-11-22 17:27:36 +0900

    Initial commit

---
+++ app/src/main/java/com/lion/team7_shopping_mall/MainActivity.kt
@@ -0,0 +1,77 @@
+package com.lion.team7_shopping_mall
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.commit
+import com.google.android.material.transition.MaterialSharedAxis
+import com.lion.team7_shopping_mall.databinding.ActivityMainBinding
+import com.lion.team7_shopping_mall.fragment.ShowClothesFragment
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var activityMainBinding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        replaceFragment(
+            FragmentName.SHOW_CLOTHES_FRAGMENT,
+            false,
+            true,
+            null,
+        )
+    }
+
+    // 프래그먼트를 교체하는 함수
+    fun replaceFragment(fragmentName: FragmentName, isAddToBackStack:Boolean, animate:Boolean, dataBundle: Bundle?){
+        // 프래그먼트 객체
+        val newFragment = when(fragmentName){
+            FragmentName.SHOW_CLOTHES_FRAGMENT -> ShowClothesFragment()
+        }
+
+        // bundle 객체가 null이 아니라면
+        if(dataBundle != null){
+            newFragment.arguments = dataBundle
+        }
+
+        // 프래그먼트 교체
+        supportFragmentManager.commit {
+
+            if(animate) {
+                newFragment.exitTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ true)
+                newFragment.reenterTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ false)
+                newFragment.enterTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ true)
+                newFragment.returnTransition = MaterialSharedAxis(MaterialSharedAxis.X, /* forward= */ false)
+            }
+
+            replace(R.id.fragmentContainerViewMain, newFragment)
+            if(isAddToBackStack){
+                addToBackStack(fragmentName.str)
+            }
+        }
+    }
+
+    // 프래그먼트를 BackStack에서 제거하는 메서드
+    fun removeFragment(fragmentName: FragmentName){
+        supportFragmentManager.popBackStack(fragmentName.str, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+    }
+}
+
+// 프래그먼트들을 나타내는 값들
+enum class FragmentName(var number:Int, var str:String){
+    SHOW_CLOTHES_FRAGMENT(3, "ShowClothesFragment")
+}
---
+++ app/src/main/java/com/lion/team7_shopping_mall/fragment/ShowClothesFragment.kt
@@ -0,0 +1,55 @@
+package com.lion.team7_shopping_mall.fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.lion.team7_shopping_mall.R
+import com.lion.team7_shopping_mall.databinding.FragmentShowClothesBinding
+
+class ShowClothesFragment : Fragment() {
+
+    lateinit var fragmentShowClothesBinding: FragmentShowClothesBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        fragmentShowClothesBinding = FragmentShowClothesBinding.inflate(inflater)
+
+        settingImageViewTextView()
+        settingToolbar()
+
+        return fragmentShowClothesBinding.root
+    }
+
+    fun settingToolbar() {
+        fragmentShowClothesBinding.apply {
+            toolbarShowClothes.title = "옷 정보"
+
+            toolbarShowClothes.setNavigationIcon(R.drawable.arrow_back_24px)
+            toolbarShowClothes.setNavigationOnClickListener {
+
+            }
+
+            toolbarShowClothes.setOnMenuItemClickListener {
+                when(it.itemId) {
+                    R.id.itemShowClothesModify -> imageViewShowClothesImage.setImageResource(R.drawable.padding_image)
+                }
+
+                true
+            }
+        }
+    }
+
+    fun settingImageViewTextView() {
+        fragmentShowClothesBinding.apply {
+            imageViewShowClothesImage.setImageResource(R.drawable.padding_image)
+            textViewShowClothesName.setText("패딩")
+        }
+    }
+
+
+
+}
